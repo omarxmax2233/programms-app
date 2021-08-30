ece91df7bc716f9b73f8136917230ff2e378847c
omarxmax2233 <89490321+omarxmax2233@users.noreply.github.com>
2021-08-30 02:32:02 +0200

    Add files via upload

---
+++ dice_game.py
@@ -0,0 +1,90 @@
+import random
+
+min_num = int(input("enter the mini number : "))
+max_num = int(input("enter the max number : "))
+max_chance = int(input("how many chance of dice you want from 1 to 10 : "))
+
+
+if max_chance == 1:
+    print(random.randint(min_num, max_num))
+
+if max_chance == 2:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 3:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 4:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+if max_chance == 5:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 6:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 7:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 8:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+if max_chance == 9:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+
+
+
+
+
+if max_chance == 10:
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+    print(random.randint(min_num, max_num))
+
+    time.sleep(22)
+
+    exit()
